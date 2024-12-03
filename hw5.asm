fcd853bd839053e657e9c9adddd65f4433829db6
Ian Kaufman <Kaufmanian49@gmail.com>
2024-12-03 22:26:41 +0000

    orient

--- hw5.asm
+++ hw5.asm
@@ -227,7 +227,6 @@ out_of_bounds:
 
 
 
-
 T_orientation4:
     move $a0, $s5          # row
     move $a1, $s6          # col
@@ -244,20 +243,20 @@ T_orientation4:
 
     move $a0, $s5          # row
     move $a1, $s6
-    addi $a1, $a1, 2       # col + 2
+    addi $a1, $a1, -1      # col - 1 (shift to the left of the center)
     move $a2, $s1
     jal place_tile          # place the third tile
     or $s2, $s2, $v0       # accumulate error
 
     move $a0, $s5
-    addi $a0, $a0, 1       # row + 1
+    addi $a0, $a0, 1       # row + 1 (below the center column)
     move $a1, $s6
-    addi $a1, $a1, 1       # col + 1
     move $a2, $s1
     jal place_tile          # place the fourth tile
     or $s2, $s2, $v0       # accumulate error
 
     j piece_done            # jump to piece_done label
+
 
 
 
